89cbb4f52f32425b2648fd2501bc962d2ad1a935
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-09 17:37:21 +0800

    Fix of tabulation and added comment

--- main/multithread/insertUpdateEntry_safe.sql
+++ main/multithread/insertUpdateEntry_safe.sql
@@ -1,7 +1,6 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUpdateEntry_safe`(entry_id varchar(100), value_field_01 varchar(100), 
 value_field_02 varchar(100), value_field_03 double, value_field_04 double, value_field_05 varchar(100))
 BEGIN
-
 	DECLARE number_of_matching_entries int;
 	
 	DECLARE exit handler for sqlexception
@@ -9,38 +8,36 @@ BEGIN
 			-- ERROR
 		ROLLBACK;
 	END;
-
+	
 	DECLARE exit handler for sqlwarning
 		BEGIN
 			-- WARNING
 		ROLLBACK;
 	END;
 	
+	/* Make all the declarations before starting the transaction*/
 	START TRANSACTION;
-    
-    SELECT count(*) INTO number_of_matching_entries 
+	
+	SELECT count(*) INTO number_of_matching_entries 
 	FROM table_to_update
 	WHERE table_id=entry_id;
-    
-    IF number_of_matching_entries = 0 THEN
+	
+	IF number_of_matching_entries = 0 THEN
 		/* need to insert */
-        INSERT INTO table_to_update (`table_id`, `table_field_01`, `table_field_02`, 
-							`table_field_03`, `table_field_04`, `table_field_05`) 
+		INSERT INTO table_to_update (`table_id`, `table_field_01`, `table_field_02`, `table_field_03`, `table_field_04`, `table_field_05`) 
 		VALUES (entry_id, value_field_01, value_field_02, value_field_03, value_field_04, value_field_05);
-        
-    ELSE
+	ELSE
 		/* need to update */
-        UPDATE table_to_update 
-        SET table_field_01=value_field_01, 
+		UPDATE table_to_update 
+		SET table_field_01=value_field_01, 
 			table_field_02=value_field_02,
-            table_field_03=value_field_03,
-            table_field_04=value_field_04,
-            table_field_05=value_field_05
-		WHERE 	table_id=entry_id;
-        
-        
-    END IF;
+			table_field_03=value_field_03,
+			table_field_04=value_field_04,
+			table_field_05=value_field_05
+		WHERE	table_id=entry_id;
+	
+	END IF;
 	
 	COMMIT;
-
+	
 END
